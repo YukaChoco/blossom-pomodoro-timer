2dcece42737494f59cf0cae3e77bca68f9849ee2
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-29 18:44:53 +0900

    いとこのためのタイマーが使えそうだったのでコードコピペ

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,5 +1,31 @@
 import styles from "./timer.module.css";
 
-export default function Timer() {
-  return <div className={styles.timer}>25:00</div>;
+export default function Timer({
+  currentTime,
+  isStudying,
+}: {
+  currentTime: number;
+  isStudying: boolean;
+}) {
+  const colorStyles = {
+    border: `4px solid ${isStudying ? "rgb(6, 73, 20)" : "rgb(6, 73, 20)"}`,
+    backgroundColor: isStudying ? "rgb(6, 73, 20)" : "rgb(192, 236, 197)",
+    color: isStudying ? "white" : "rgb(6, 73, 20)",
+  };
+
+  return (
+    <div style={colorStyles} className={styles.timer}>
+      <span>
+        {Math.floor(currentTime / 600) < 10
+          ? "0"
+          : Math.floor(currentTime / 600)}
+      </span>
+      <span>{Math.floor((currentTime / 60) % 10)}</span>
+      <span>:</span>
+      <span>
+        {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
+      </span>
+      <span>{(currentTime % 60) % 10}</span>
+    </div>
+  );
 }
