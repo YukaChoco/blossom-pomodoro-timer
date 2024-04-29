7879a019d35c62190a21af7523f2ff08d39e84eb
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-29 13:00:29 +0900

    一旦できた

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,5 +1,21 @@
-import styles from "./timer.module.css";
+export default function Timer({
+  currentTime,
+  isStudying,
+}: {
+  currentTime: number;
+  isStudying: boolean;
+}) {
+  const styles = {
+    padding: "40px 80px",
+    border: `4px solid ${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
+    fontSize: "4rem",
+    color: `${isStudying ? "rgb(255, 44, 79)" : "rgb(0, 0, 150)"}`,
+  };
 
-export default function Timer() {
-  return <div className={styles.timer}>25:00</div>;
+  return (
+    <div style={styles}>
+      {Math.floor(currentTime / 60)}:
+      {currentTime % 60 < 10 ? `0${currentTime % 60}` : currentTime % 60}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,58 @@
+"use client";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Timer from "./components/Timer";
 import styles from "./page.module.css";
 
 export default function Home() {
+  // タイマーの初期値を5分に設定する
+  const initialTime = 1 * 10;
+  const [currentTime, setCurrentTime] = useState<number>(initialTime);
+  const [isStudying, setIsStudying] = useState<boolean>(true);
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+
+  console.log(currentTime);
+
+  // 5分カウントダウンタイマー
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (isTimerRunning) {
+        if (currentTime <= 0) {
+          setCurrentTime(initialTime);
+          setIsStudying((prev) => !prev);
+          console.log("change");
+        } else {
+          setCurrentTime((prev) => prev - 1);
+          console.log("counted down");
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [isTimerRunning, currentTime]);
+
   return (
     <main className={styles.main}>
       <Header />
-      <Timer />
-      <a href="/result">go to result</a>
+      <Timer currentTime={currentTime} isStudying={isStudying} />
+      {isTimerRunning ? (
+        isStudying ? (
+          <div>勉強中！頑張れめいちゃん！</div>
+        ) : (
+          <div>〜休憩中〜</div>
+        )
+      ) : (
+        <button onClick={() => setIsTimerRunning((pre) => !pre)}>
+          勉強を始める
+        </button>
+      )}
+      {isTimerRunning && !isStudying && (
+        <img height={250} src="/image.png" alt="JO1の写真" />
+      )}
+
+      {/* <a href="/result">go to result</a> */}
     </main>
   );
 }
