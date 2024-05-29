0d36dd403aee4858d90eb6d981170b962f1b6f86
maruhashiyukina <sachiyuki14@icloud.com>
2024-05-29 14:35:42 +0900

    'いったん'

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,4 +1,5 @@
 import styles from "./timer.module.css";
+import React, { useState, useEffect, use } from "react";
 
 export default function Timer({
   currentTime,
@@ -9,23 +10,24 @@ export default function Timer({
 }) {
   const colorStyles = {
     border: `4px solid ${isStudying ? "rgb(6, 73, 20)" : "rgb(6, 73, 20)"}`,
-    backgroundColor: isStudying ? "rgb(6, 73, 20)" : "rgb(192, 236, 197)",
-    color: isStudying ? "white" : "rgb(6, 73, 20)",
   };
+
+  const [setcount, setSetcount] = useState(1);
 
   return (
     <div style={colorStyles} className={styles.timer}>
-      <span>
-        {Math.floor(currentTime / 600) < 10
-          ? "0"
-          : Math.floor(currentTime / 600)}
-      </span>
-      <span>{Math.floor((currentTime / 60) % 10)}</span>
-      <span>:</span>
-      <span>
-        {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
-      </span>
-      <span>{(currentTime % 60) % 10}</span>
+      <div className={styles.set}>{setcount}セット目</div>
+        <span>
+          {Math.floor(currentTime / 600) < 10
+            ? "0"
+            : Math.floor(currentTime / 600)}
+        </span>
+        <span>{Math.floor((currentTime / 60) % 10)}</span>
+        <span>:</span>
+        <span>
+          {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
+        </span>
+        <span>{(currentTime % 60) % 10}</span>
     </div>
   );
 }
