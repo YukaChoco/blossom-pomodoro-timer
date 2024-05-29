570554fe1f992adb22567537ba81d2d9d8fe4001
YukaChoco <sugar.ore.yk@gmail.com>
2024-05-29 13:35:31 +0900

    ロジックをhooksに移動

---
+++ app/hooks/useTimer.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+const useTimer = () => {
+  // タイマーの初期値を5分に設定する
+  const [initialStudyMinute, setInitialStudyMinute] = useState<number>(5);
+  const [initialBreakMinute, setInitialBreakMinute] = useState<number>(5);
+  const initialStudyTime = initialStudyMinute * 60;
+  const initialBreakTime = initialBreakMinute * 60;
+  const [currentTime, setCurrentTime] = useState<number>(
+    initialStudyMinute * 60
+  );
+  const [isStudying, setIsStudying] = useState<boolean>(true);
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+
+  // 5分カウントダウンタイマー
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (isTimerRunning) {
+        if (currentTime <= 0) {
+          if (isStudying) {
+            setCurrentTime(initialBreakTime);
+          } else {
+            setCurrentTime(initialStudyTime);
+          }
+          setIsStudying((prev) => !prev);
+        } else {
+          setCurrentTime((prev) => prev - 1);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [isTimerRunning, currentTime]);
+
+  return {
+    initialStudyMinute,
+    initialBreakMinute,
+    currentTime,
+    isStudying,
+    isTimerRunning,
+    initialStudyTime,
+    setInitialStudyMinute,
+    setInitialBreakMinute,
+    setCurrentTime,
+    setIsTimerRunning,
+  };
+};
+
+export default useTimer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,22 @@
 "use client";
-import { useEffect, useState } from "react";
+import useTimer from "./hooks/useTimer";
 import Header from "./components/Header";
 import Timer from "./components/Timer";
 import styles from "./page.module.css";
 
 export default function Home() {
-  // タイマーの初期値を5分に設定する
-  const [initialStudyMinute, setInitialStudyMinute] = useState<number>(5);
-  const [initialBreakMinute, setInitialBreakMinute] = useState<number>(5);
-  const initialStudyTime = initialStudyMinute * 60;
-  const initialBreakTime = initialBreakMinute * 60;
-  const [currentTime, setCurrentTime] = useState<number>(
-    initialStudyMinute * 60
-  );
-  const [isStudying, setIsStudying] = useState<boolean>(true);
-  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
-
-  // 5分カウントダウンタイマー
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      if (isTimerRunning) {
-        if (currentTime <= 0) {
-          if (isStudying) {
-            setCurrentTime(initialBreakTime);
-          } else {
-            setCurrentTime(initialStudyTime);
-          }
-          setIsStudying((prev) => !prev);
-        } else {
-          setCurrentTime((prev) => prev - 1);
-        }
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(timerId);
-    };
-  }, [isTimerRunning, currentTime]);
+  const {
+    initialStudyMinute,
+    initialBreakMinute,
+    currentTime,
+    isStudying,
+    isTimerRunning,
+    initialStudyTime,
+    setInitialStudyMinute,
+    setInitialBreakMinute,
+    setIsTimerRunning,
+    setCurrentTime,
+  } = useTimer();
 
   return (
     <main className={styles.main}>
