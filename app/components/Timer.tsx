64e484c49258311098fff4a96608e7d251a0703f
maruhashiyukina <sachiyuki14@icloud.com>
2024-06-12 20:25:01 +0900

    'timer画像調整'

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -36,53 +36,53 @@ export default function Timer({
 
   return (
     <div className={styles.sakuracontainer}>
-    <img src="./Frame 2.png"></img>
-    <div className={styles.container}>
-      <CircularProgress
-        sx={{
-          "--CircularProgress-size": "24rem",
-          position: "absolute",
-          "--CircularProgress-trackColor": "#797979",
-          "--CircularProgress-progressColor": "#FFD600"
-        }}
-        thickness={20}
-        determinate value={progress}
-      />
-      <div className={styles.timer}>
-        <div>
-          <p>{setcount}セット目</p>
+      <img src="./Frame 2.png" />
+      <div className={styles.container}>
+        <CircularProgress
+          sx={{
+            "--CircularProgress-size": "24rem",
+            position: "absolute",
+            "--CircularProgress-trackColor": "#797979",
+            "--CircularProgress-progressColor": "#FFD600"
+          }}
+          thickness={20}
+          determinate value={progress}
+        />
+        <div className={styles.timer}>
+          <div>
+            <p>{setcount}セット目</p>
+          </div>
+          <div>
+            <span>
+              {Math.floor(currentTime / 600) < 10
+                ? "0"
+                : Math.floor(currentTime / 600)}
+            </span>
+            <span>{Math.floor((currentTime / 60) % 10)}</span>
+            <span>:</span>
+            <span>
+              {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
+            </span>
+            <span>{(currentTime % 60) % 10}</span>
+          </div>
+          {mode === Mode.BeforeStart ? (
+            // 勉強開始前
+            <button className={styles.timerButton} onClick={startTimer}>
+              <img src="/playButton.svg"></img>
+            </button>
+          ) : isTimerRunning ? (
+            // 勉強中の一時停止
+            <button className={styles.timerButton} onClick={stopTimer}>
+              <img src="/stopButton.svg"></img>
+            </button>
+          ) : (
+            // 勉強中の再開
+            <button className={styles.timerButton} onClick={restartTimer}>
+              <img src="/playButton.svg"></img>
+            </button>
+          )}
         </div>
-        <div>
-          <span>
-            {Math.floor(currentTime / 600) < 10
-              ? "0"
-              : Math.floor(currentTime / 600)}
-          </span>
-          <span>{Math.floor((currentTime / 60) % 10)}</span>
-          <span>:</span>
-          <span>
-            {currentTime % 60 < 10 ? "0" : Math.floor((currentTime % 60) / 10)}
-          </span>
-          <span>{(currentTime % 60) % 10}</span>
-        </div>
-        {mode === Mode.BeforeStart ? (
-          // 勉強開始前
-          <button className={styles.timerButton} onClick={startTimer}>
-            <img src="/playButton.svg"></img>
-          </button>
-        ) : isTimerRunning ? (
-          // 勉強中の一時停止
-          <button className={styles.timerButton} onClick={stopTimer}>
-            <img src="/stopButton.svg"></img>
-          </button>
-        ) : (
-          // 勉強中の再開
-          <button className={styles.timerButton} onClick={restartTimer}>
-            <img src="/playButton.svg"></img>
-          </button>
-        )}
       </div>
-    </div>
     </div>
   );
 }
