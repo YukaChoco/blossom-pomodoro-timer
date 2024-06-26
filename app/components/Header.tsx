9771d6108112fd6819114c19e9748a66d31ecc50
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2024-06-26 14:14:16 +0900

    ホームに戻るようにしました～

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,14 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
 
-export default function Header({finishStudy} : {finishStudy: () => void}) {
+export default function Header({ finishStudy }: { finishStudy: () => void }) {
   return (
-    <AppBar sx={{backgroundColor: '#F6BEC9' }}>
+    <AppBar sx={{ backgroundColor: '#F6BEC9' }}>
       <Toolbar>
-      <img src="/blossom_icon.svg" alt="Blossom Icon" style={{ marginRight: 16, height: 42 }} />
-        <span style={{fontSize: 42}}>PomodoroTimer</span>
-        <Button sx={{margin:  '0 0 0 auto', color: '#fff'}} onClick={finishStudy}>勉強を<br />終える</Button>
+        <a href="/">
+          <img src="/blossom_icon.svg" alt="Blossom Icon" style={{ marginRight: 16, height: 42 }} />
+          <span style={{ fontSize: 42 }} >PomodoroTimer</span>
+        </a>
+        <Button sx={{ margin: '0 0 0 auto', color: '#fff' }} onClick={finishStudy}>勉強を<br />終える</Button>
       </Toolbar>
     </AppBar>
   )
