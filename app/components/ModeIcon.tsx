83bff13b52f9ecccbfd1cd13b6fb9686c17a2700
maruhashiyukina <sachiyuki14@icloud.com>
2024-06-26 15:44:18 +0900

    アイコンが徐々に変化します

--- app/components/ModeIcon.tsx
+++ app/components/ModeIcon.tsx
@@ -2,21 +2,20 @@ import { Mode } from "../types/mode";
 import styles from "./modeIcon.module.css";
 
 export default function ModeIcon({ mode }: { mode: Mode }) {
-  if (mode === Mode.Studying){
-    return (
-      <div className={styles.modeIcon}>
-        <img src="./working_image.svg" width="80%" />
-        <h3 className={styles.h3study}>Studying...</h3>
-      </div>
-    )
-  } else if (mode === Mode.Breaking) {
-    return (
-      <div className={styles.modeIcon}>
-        <img src="./breakimage.svg" width="80%" />
-        <h3 className={styles.h3break}>Break time...</h3>
-      </div>
-    )
-  } else {
-    return 0;
-  }
+  return (
+    <>
+    <div className={styles.modeIcon} style={{opacity: mode === Mode.Studying ? 1 : 0}}>
+      <img src="./working_image.svg" width="80%" />
+      <h3 className={styles.h3study}>Studying...</h3>
+    </div>
+    <div className={styles.modeIcon} style={{opacity: mode === Mode.Breaking ? 1 : 0}}>
+      <img src="./breakimage.svg" width="80%" />
+      <h3 className={styles.h3break}>Break time...</h3>
+    </div>
+    <div className={styles.modeIcon} style={{opacity: mode === Mode.Finished ? 1 : 0}}>
+      <img src="./finished_image.svg" width="80%" />
+      <h3 className={styles.h3finish}>Finished!</h3>
+    </div>
+    </>
+  )
 }
