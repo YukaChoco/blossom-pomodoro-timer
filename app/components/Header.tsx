0b71278cb3d4ae70d2ec6ac52700b29120541c36
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2024-06-26 14:23:54 +0900

    Imageタグ設定しなおしました！

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { AppBar, Toolbar, Button } from "@mui/material";
 
 export default function Header({ finishStudy }: { finishStudy: () => void }) {
@@ -5,7 +6,7 @@ export default function Header({ finishStudy }: { finishStudy: () => void }) {
     <AppBar sx={{ backgroundColor: '#F6BEC9' }}>
       <Toolbar>
         <a href="/">
-          <img src="/blossom_icon.svg" alt="Blossom Icon" style={{ marginRight: 16, height: 42 }} />
+          <Image height="42" width="42" src="/blossom_icon.svg" alt="Blossom Icon" style={{ marginTop: 5, marginRight: 16 }} />
           <span style={{ fontSize: 42 }} >PomodoroTimer</span>
         </a>
         <Button sx={{ margin: '0 0 0 auto', color: '#fff' }} onClick={finishStudy}>勉強を<br />終える</Button>
