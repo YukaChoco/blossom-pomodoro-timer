d53cb2763d93bb1eaffa778558beefbb52b7cacc
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2024-06-26 13:44:25 +0900

    ヘッダー完成～

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,13 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+
 export default function Header() {
-  return <div>PomodoroTimer</div>;
+  return (
+    <AppBar sx={{backgroundColor: '#F6BEC9' }}>
+      <Toolbar>
+      <img src="/blossom_icon.svg" alt="Blossom Icon" style={{ marginRight: 16, height: 42 }} />
+        <span style={{fontSize: 42}}>PomodoroTimer</span>
+        <Button sx={{margin:  '0 0 0 auto', color: '#fff'}}>勉強を<br />終える</Button>
+      </Toolbar>
+    </AppBar>
+  )
 }
